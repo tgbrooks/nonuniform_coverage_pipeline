a627e821acfc2d06286c53d95e6d7b5fe70e1d0c
tgbrooks <tgbrooks@gmail.com>
2026-03-24 13:10:43 -0400

    Plot GVS by PCR cycle count

--- src/scratch/global_valley_scores.R
+++ src/scratch/global_valley_scores.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 
 
+# UHR degraded (RIN score association)
 sample_info <- read_tsv("results/UHR_degraded.sample_info.txt")
 gvs <- read_tsv("results/UHR_degraded/corrected_global_valley_score.txt") |>
     left_join(sample_info, by = join_by(sample == ID))
@@ -13,3 +14,15 @@ ggplot(gvs, aes(x=rin_score, y=global_valley_score)) +
          y = "Global valley score",
     )
 ggsave("results/scratch/UHR_degraded.global_valley_scores.png", width=3, height=3)
+
+# Testis (PCR cycle count)
+gvs <- read_tsv("results/testis/global_valley_scores.txt")
+
+ggplot(gvs, aes(x=PCR_cycle_count, y=global_valley_score)) +
+    geom_smooth(method='lm', formula= y~x) +
+    geom_point() +
+    labs(
+         x = "PCR cycle number",
+         y = "Global valley score",
+    )
+ggsave("results/scratch/testis.PCR_cycle_number.global_valley_scores.png", width=3, height=3)
