ad549b0b8b0be04154d337c0e936857c21618f19
tgbrooks <tgbrooks@gmail.com>
2026-03-25 15:04:18 -0400

    Update pcr dupe rate

---
+++ src/scripts/get_pcr_dupe_rate_coverage.py
@@ -0,0 +1,226 @@
+import pathlib
+import sqlite3
+import numpy as np
+import polars as pl
+
+cycle_counts_by_id = {
+    "SRX3357955": 8,
+    "SRX3357956": 9,
+    "SRX3357957": 10,
+    "SRX3357958": 11,
+    "SRX3357959": 13,
+}
+
+
+sample_ids = sorted(snakemake.params.sample_ids)
+
+dupe_rate_files = snakemake.input.dupe_rate
+temp = []
+for dupe_rate_file, sample_id in zip(dupe_rate_files, sample_ids):
+    dupe_rate = pl.read_csv(
+        dupe_rate_file,
+        has_header=False,
+        new_columns=["chrom", "start", "end", "value"],
+        separator="\t",
+        dtypes=[pl.Utf8, pl.Int32, pl.Int32, pl.Float32],
+    ).with_columns(sample_id=pl.lit(sample_id))
+    temp.append(dupe_rate)
+dupe_rate = pl.concat(temp)
+
+cov_files = snakemake.input.coverage
+temp = []
+for cov_file, sample_id in zip(cov_files, sample_ids):
+    cov = pl.read_csv(
+        cov_file,
+        has_header=False,
+        new_columns=["chrom", "start", "end", "value"],
+        separator="\t",
+        dtypes=[pl.Utf8, pl.Int32, pl.Int32, pl.Int32],
+    ).with_columns(sample_id=pl.lit(sample_id))
+    temp.append(cov)
+cov = pl.concat(temp)
+
+
+annotation_db = snakemake.input.annotation
+with sqlite3.connect(annotation_db) as conn:
+    exons = pl.read_database("SELECT * FROM exon", conn)
+    tx2exon = pl.read_database("SELECT * FROM tx2exon", conn)
+    gene = pl.read_database("SELECT * FROM gene", conn)
+    tx = pl.read_database("SELECT * FROM tx", conn)
+
+
+# compile the list of transcripts we'll plot
+transcripts_file = snakemake.input.transcripts
+## list in all the mitochondrial transcripts
+mt_genes = gene.filter(pl.col("gene_name").str.starts_with("mt-"))["gene_id"]
+mt_tx = tx.filter(pl.col("gene_id").is_in(mt_genes))
+transcripts = pl.concat(
+    (
+        # Hand-picked genes
+        pl.DataFrame(
+            {
+                "gene_id": [
+                    "ENSMUSG00000064339",
+                    "ENSMUSG00000032554",
+                    "ENSMUSG00000002985",
+                    "ENSMUSG00000072849",
+                    "ENSMUSG00000022868",
+                    "ENSMUSG00000028001",
+                    "ENSMUSG00000064337",
+                    "ENSMUSG00000064370",
+                    "ENSMUSG00000064345",
+                ],
+                "transcript_id": [
+                    "ENSMUST00000082390",
+                    "ENSMUST00000112645",
+                    "ENSMUST00000174064",
+                    "ENSMUST00000085054",
+                    "ENSMUST00000023583",
+                    "ENSMUST00000166581",
+                    "ENSMUST00000082388",
+                    "ENSMUST00000082421",
+                    "ENSMUST00000082396",
+                ],
+            }
+        ),
+        # mitochondrial transcripts
+        mt_tx.select("gene_id", pl.col("tx_id").alias("transcript_id")),
+        # Our standard set of 100 single-isoform genes
+        pl.read_csv(transcripts_file, separator="\t").select(
+            "gene_id", "transcript_id"
+        ),
+    )
+).filter(pl.col("gene_id").is_first_distinct())
+
+
+def gene_to_exon_pos(gene_id):
+    gene_info = gene.filter(gene_id=gene_id)
+    chrom = gene_info["seq_name"][0]
+    tx_id = transcripts.filter(gene_id=gene_id)["transcript_id"][0]
+    these_exons = exons.join(tx2exon.filter(tx_id=tx_id), on="exon_id").sort(
+        "exon_seq_start"
+    )
+
+    # Base-pair positions of the gene's exons in genomic coordinates
+    pos = pl.concat(
+        [
+            pl.DataFrame(
+                {
+                    "pos": np.arange(start - 1, end, dtype="int32"),
+                    "chrom": chrom,
+                }
+            )
+            for (start, end) in these_exons.select(
+                "exon_seq_start", "exon_seq_end"
+            ).iter_rows()
+        ]
+    )
+    return pos
+
+
+def get_cov_and_dupe(gene_id, cov, dupe_rate):
+    tx_id = str(transcripts.filter(gene_id=gene_id)["transcript_id"][0])
+    chrom = gene.filter(gene_id=gene_id)["seq_name"][0]
+    these_exons = exons.join(tx2exon.filter(tx_id=tx_id), on="exon_id").sort(
+        "exon_seq_start"
+    )
+
+    # Base-pair positions of the gene's exons in genomic coordinates
+    pos = pl.concat(
+        [
+            pl.DataFrame(
+                {
+                    "pos": np.arange(start - 1, end, dtype="int32"),
+                    "chrom": chrom,
+                }
+            )
+            for (start, end) in these_exons.select(
+                "exon_seq_start", "exon_seq_end"
+            ).iter_rows()
+        ]
+    )
+
+    # Find the read depth/dupe_rates for each position
+    # note that the cov/dupe_rate files give a range (start-end)
+    # and so we have to join_asof to find the right range
+    this_cov = pos.join_asof(
+        cov,
+        left_on="pos",
+        right_on="start",
+        by="chrom",
+    ).with_columns(
+        value=pl.when(pl.col("pos") >= pl.col("end"))
+        .then(0)
+        .otherwise(pl.col("value")),
+    )
+    this_dupe_rate = pos.join_asof(
+        dupe_rate, left_on="pos", right_on="start", by="chrom"
+    ).with_columns(
+        value=pl.when(pl.col("pos") >= pl.col("end"))
+        .then(0)
+        .otherwise(pl.col("value")),
+    )
+
+    both = (
+        this_cov.select("pos", pl.col("value").alias("cov"))
+        .join(
+            this_dupe_rate.select("pos", pl.col("value").alias("dupe_rate")),
+            on="pos",
+            validate="1:1",
+        )
+        .with_row_count(name="loc")
+        .with_columns(
+            transcript_id=pl.lit(tx_id),
+        )
+    )
+    return both
+
+
+temp = list()
+for sample_id in sample_ids:
+    coverage_file = (
+        f"data/{sample_id}/pcr_dupe_rate_coverage/{sample_id}.deduped_coverage.bed"
+    )
+    dupe_rate_file = f"data/{sample_id}/pcr_dupe_rate_coverage/{sample_id}.pcr_dupe_rate_coverage.bed"
+
+    all_dupe_rate = pl.read_csv(
+        dupe_rate_file,
+        has_header=False,
+        new_columns=["chrom", "start", "end", "value"],
+        separator="\t",
+        dtypes=[pl.Utf8, pl.Int32, pl.Int32, pl.Float32],
+    )
+    all_coverage = pl.read_csv(
+        coverage_file,
+        has_header=False,
+        new_columns=["chrom", "start", "end", "value"],
+        separator="\t",
+        dtypes=[pl.Utf8, pl.Int32, pl.Int32, pl.Int32],
+    )
+
+    for gene_id in transcripts["gene_id"]:
+        temp.append(
+            get_cov_and_dupe(gene_id, all_coverage, all_dupe_rate).with_columns(
+                sample_id=pl.lit(sample_id),
+                gene_id=pl.lit(gene_id),
+                cycle_count=pl.lit(cycle_counts_by_id[sample_id]),
+            )
+        )
+
+data = pl.concat(temp)
+data.write_csv(snakemake.output.full_cov, separator="\t")
+
+summed_data = (
+    # No actual summing for this dataset since only one library per sample
+    data.group_by("cycle_count", "sample_id", "gene_id", "transcript_id", "pos", "loc")
+    .agg(
+        cov=pl.col("cov").sum(),
+        # combine dupe rates across the different samples, weighted sum according to the depth of coverage
+        dupe_rate=(
+            (pl.col("cov") * pl.col("dupe_rate")).sum() / pl.col("cov").sum()
+        ).fill_nan(pl.lit(None)),
+    )
+    .sort(["transcript_id", "loc"])
+    .with_columns(mask=pl.col("cov") > 100)
+)
+summed_data.write_csv(snakemake.output.summed_cov, separator="\t")
--- src/scripts/plot_pcr_dupe_rate_coverage.py
+++ src/scripts/plot_pcr_dupe_rate_coverage.py
@@ -2,6 +2,8 @@ import pathlib
 import sqlite3
 import numpy as np
 import polars as pl
+import scipy.ndimage
+import scipy.stats
 import matplotlib as mpl
 import matplotlib.pyplot as pyplot
 
@@ -13,162 +15,89 @@ cycle_counts_by_id = {
     "SRX3357959": 13,
 }
 
+select_genes = [
+    "ENSMUSG00000000594",
+    "ENSMUSG00000007836",
+    "ENSMUSG00000032437",
+]
 
-outdir = pathlib.Path(snakemake.output.outdir)
+outdir = pathlib.Path("results/testis/pcr_dupe_rate_coverage_plot/")
 outdir.mkdir(exist_ok=True)
 
-sample_ids = snakemake.params.sample_ids
+data = pl.read_csv(
+    "results/testis/pcr_dupe_rate_coverage.aggregated.txt", separator="\t"
+)
+sample_ids = sorted(data["sample_id"].unique())
 
-dupe_rate_files = snakemake.input.dupe_rate
-temp = []
-for (dupe_rate_file, sample_id) in zip(dupe_rate_files, sample_ids):
-    dupe_rate = pl.read_csv(dupe_rate_file, has_header=False, new_columns=["chrom", "start", "end", "value"], separator="\t", dtypes=[pl.Utf8, pl.Int32, pl.Int32, pl.Float32]) \
-        .with_columns(sample_id = pl.lit(sample_id))
-    temp.append(dupe_rate)
-dupe_rate = pl.concat(temp)
-
-cov_files = snakemake.input.coverage
-temp = []
-for (cov_file, sample_id) in zip(cov_files, sample_ids):
-    cov = pl.read_csv(cov_file, has_header=False, new_columns=["chrom", "start", "end", "value"], separator="\t", dtypes=[pl.Utf8, pl.Int32, pl.Int32, pl.Int32]) \
-        .with_columns(sample_id = pl.lit(sample_id))
-    temp.append(cov)
-cov = pl.concat(temp)
-
-
-
-annotation_db = snakemake.input.annotation
+annotation_db = "data/Mus_musculus.GRCm38.102.gtf.sqlite"
 with sqlite3.connect(annotation_db) as conn:
-    exons = pl.read_database("SELECT * FROM exon", conn)
-    tx2exon = pl.read_database("SELECT * FROM tx2exon", conn)
+    # exons = pl.read_database("SELECT * FROM exon", conn)
+    # tx2exon = pl.read_database("SELECT * FROM tx2exon", conn)
     gene = pl.read_database("SELECT * FROM gene", conn)
     tx = pl.read_database("SELECT * FROM tx", conn)
 
 
-# compile the list of transcripts we'll plot 
-transcripts_file = snakemake.input.transcripts
-## list in all the mitochondrial transcripts
-mt_genes = gene.filter(pl.col("gene_name").str.starts_with("mt-"))['gene_id']
-mt_tx = tx.filter(pl.col("gene_id").is_in(mt_genes))
-transcripts = pl.concat((
-        # Hand-picked genes
-        pl.DataFrame({
-            "gene_id": ["ENSMUSG00000064339","ENSMUSG00000032554", "ENSMUSG00000002985", "ENSMUSG00000072849", "ENSMUSG00000022868", "ENSMUSG00000028001", "ENSMUSG00000064337", "ENSMUSG00000064370", "ENSMUSG00000064345"],
-            "transcript_id": ["ENSMUST00000082390", "ENSMUST00000112645", "ENSMUST00000174064", "ENSMUST00000085054", "ENSMUST00000023583", "ENSMUST00000166581", "ENSMUST00000082388", "ENSMUST00000082421", "ENSMUST00000082396"]
-        }),
-        # mitochondrial transcripts
-        mt_tx.select("gene_id", pl.col("tx_id").alias("transcript_id")),
-        # Our standard set of 100 single-isoform genes
-        pl.read_csv(transcripts_file, separator="\t").select("gene_id", "transcript_id"),
-)).filter(pl.col("gene_id").is_first_distinct())
-
-#select_genes = ["ENSMUSG00000064351","ENSMUSG00000064367", "ENSMUSG00000064370",] # All mitochondrial
-select_genes = ["ENSMUSG00000064351", "ENSMUSG00000032968", "ENSMUSG00000000594"] # more mixed genes
-
-def gene_to_exon_pos(gene_id):
-    gene_info = gene.filter(gene_id = gene_id)
-    chrom = gene_info['seq_name'][0]
-    tx_id = transcripts.filter(gene_id = gene_id)['transcript_id'][0]
-    these_exons = (
-        exons.join(tx2exon.filter(tx_id = tx_id), on="exon_id")
-            .sort("exon_seq_start")
-    )
-
-    # Base-pair positions of the gene's exons in genomic coordinates
-    pos = pl.concat([
-        pl.DataFrame({
-            "pos": np.arange(start-1, end, dtype="int32"),
-            "chrom": chrom,
-        }) for (start, end) in these_exons.select("exon_seq_start", "exon_seq_end").iter_rows()
-    ])
-    return pos
-
-def get_cov_and_dupe_rate(gene_id, sample_id):
-    pos = gene_to_exon_pos(gene_id)
-
-    this_dupe_rate = dupe_rate.filter(pl.col("sample_id") == sample_id)
-    this_cov = cov.filter(pl.col("sample_id") == sample_id)
-
-    # Find the read depth/dupe_rates for each position
-    # note that the cov/dupe_rate files give a range (start-end)
-    # and so we have to join_asof to find the right range
-    this_cov = (pos
-        .join_asof(
-            this_cov,
-            left_on = "pos",
-            right_on = "start",
-            by = "chrom",
-        )
-        .filter(
-            pl.col('pos') < pl.col('end'),
-        )
-    )
-    this_dupe_rate = (pos
-        .join_asof(
-            this_dupe_rate,
-            left_on = "pos",
-            right_on = "start",
-            by = "chrom"
-        )
-        .filter(
-            pl.col('pos') < pl.col('end'),
-        )
-    )
-
-    both = (
-        this_cov.select('pos', pl.col('value').alias("cov"))
-            .join(this_dupe_rate.select('pos', pl.col('value').alias('dupe_rate')), on="pos", validate="1:1")
-            .with_row_index(name="loc")
-            .with_columns(mask = pl.col('cov') > 100)
-    )
-    return both
-
 ###### PLOT SELECT GENES TOGETHER
 color_by_cycle_count = {
-    cycles: mpl.colormaps['viridis']((cycles-8)/5)
-        for cycles in [8,9,10,11,13]
+    cycles: mpl.colormaps["viridis"]((cycles - 8) / 5) for cycles in [8, 9, 10, 11, 13]
 }
 handles = {}
 for format in ["norm", "raw"]:
     fig, axes = pyplot.subplots(
-        figsize=(10,4),
+        figsize=(10, 4),
         nrows=2,
         ncols=len(select_genes),
-        sharex='col',
+        sharex="col",
         gridspec_kw=dict(right=0.85),
     )
-    for (i, gene_id) in enumerate(select_genes):
-        gene_name = gene.filter(gene_id = gene_id)['gene_name'][0]
-        cov_ax = axes[0,i]
-        dupe_ax = axes[1,i]
-        all_cov_and_dupes = []
-        for sample_id in sample_ids:
-            cycle_count = cycle_counts_by_id[sample_id]
-            all_cov_and_dupes.append(
-                get_cov_and_dupe_rate(gene_id, sample_id)
-                    .with_columns(cycle_count = pl.lit(cycle_count))
-            )
-        all_cov_and_dupes = pl.concat(all_cov_and_dupes) \
-            .with_columns(
-                norm_cov = pl.col('cov') / pl.col('cov').mean().over("cycle_count"),
-                norm_dupe_rate = pl.col('dupe_rate') / pl.col('dupe_rate').mean().over("cycle_count"),
-            )
+    for i, gene_id in enumerate(select_genes):
+        gene_name = gene.filter(gene_id=gene_id)["gene_name"][0]
+        cov_ax = axes[0, i]
+        dupe_ax = axes[1, i]
+        all_cov_and_dupes = data.filter(gene_id=gene_id).with_columns(
+            norm_cov=pl.col("cov")
+            / pl.col("cov").mean().over(["sample_id", "cycle_count"]),
+            norm_dupe_rate=pl.col("dupe_rate")
+            / pl.col("dupe_rate").mean().over(["sample_id", "cycle_count"]),
+        )
 
-        for ((cycle_count,), cov_and_dupe) in all_cov_and_dupes.group_by("cycle_count"):
+        for (cycle_count,), cov_and_dupe in all_cov_and_dupes.group_by("cycle_count"):
             if format == "raw":
-                h, = cov_ax.plot(cov_and_dupe['loc'], cov_and_dupe['cov'], color=color_by_cycle_count[cycle_count], label="coverage")
+                (h,) = cov_ax.plot(
+                    cov_and_dupe["loc"],
+                    cov_and_dupe["cov"],
+                    color=color_by_cycle_count[cycle_count],
+                    label="coverage",
+                )
 
-                mask = np.array(cov_and_dupe['mask']).astype(bool)
-                masked_dupe_rate = np.array(cov_and_dupe['dupe_rate']).astype(float)
+                mask = np.array(cov_and_dupe["mask"]).astype(bool)
+                masked_dupe_rate = np.array(cov_and_dupe["dupe_rate"]).astype(float)
                 masked_dupe_rate[~mask] = float("nan")
-                dupe_ax.plot(cov_and_dupe['loc'], masked_dupe_rate, color=color_by_cycle_count[cycle_count], label="dupe rate")
+                dupe_ax.plot(
+                    cov_and_dupe["loc"],
+                    masked_dupe_rate,
+                    color=color_by_cycle_count[cycle_count],
+                    label="dupe rate",
+                )
             else:
-                h, = cov_ax.plot(cov_and_dupe['loc'], cov_and_dupe['norm_cov'], color=color_by_cycle_count[cycle_count], label="coverage")
+                (h,) = cov_ax.plot(
+                    cov_and_dupe["loc"],
+                    cov_and_dupe["norm_cov"],
+                    color=color_by_cycle_count[cycle_count],
+                    label="coverage",
+                )
 
-                mask = np.array(cov_and_dupe['mask']).astype(bool)
-                masked_dupe_rate = np.array(cov_and_dupe['norm_dupe_rate']).astype(float)
+                mask = np.array(cov_and_dupe["mask"]).astype(bool)
+                masked_dupe_rate = np.array(cov_and_dupe["norm_dupe_rate"]).astype(
+                    float
+                )
                 masked_dupe_rate[~mask] = float("nan")
-                dupe_ax.plot(cov_and_dupe['loc'], masked_dupe_rate, color=color_by_cycle_count[cycle_count], label="dupe rate")
+                dupe_ax.plot(
+                    cov_and_dupe["loc"],
+                    masked_dupe_rate,
+                    color=color_by_cycle_count[cycle_count],
+                    label="dupe rate",
+                )
             handles[cycle_count] = h
 
         if i == 0:
@@ -179,33 +108,134 @@ for format in ["norm", "raw"]:
     fig.legend(
         handles=[handles[c] for c in color_by_cycle_count.keys()],
         labels=[str(c) for c in color_by_cycle_count.keys()],
-        loc = "center right",
-        title = "PCR cycle count"
+        loc="center right",
+        title="PCR cycle count",
     )
     fig.tight_layout()
     fig.savefig(outdir / f"select_genes.{format}.svg")
     fig.savefig(outdir / f"select_genes.{format}.png")
     pyplot.close()
 
-###### PLOT INDIVIDUAL GENES
-for (gene_id, tx_id) in transcripts.iter_rows():
-    gene_name = gene.filter(gene_id = gene_id)['gene_name'][0]
+####### PLOT INDIVIDUAL GENES
+# for gene_id, tx_id in transcripts.iter_rows():
+#    gene_name = gene.filter(gene_id=gene_id)["gene_name"][0]
+#
+#    # Make the plot
+#    fig, axes = pyplot.subplots(
+#        figsize=(5, 1.5 * len(sample_ids)),
+#        nrows=len(sample_ids),
+#        layout="constrained",
+#        sharex=True,
+#        sharey=False,
+#    )
+#
+#    for ax, sample_id in zip(axes, sample_ids):
+#        both = get_cov_and_dupe_rate(gene_id, sample_id)
+#        mask = np.array(both["mask"]).astype(bool)
+#        masked_dupe_rate = np.array(both["dupe_rate"]).astype(float)
+#        masked_dupe_rate[~mask] = float("nan")
+#        (dupe_rate_lines,) = ax.plot(
+#            both["loc"], masked_dupe_rate, color="b", label="dupe rate"
+#        )
+#        ax.set_ylabel(f"{sample_id}\nPCR dupe rate")
+#        ax.set_xlabel("loc")
+#        ax_true = ax.twinx()
+#        (coverage_lines,) = ax_true.plot(
+#            both["loc"], both["cov"], color="k", label="coverage"
+#        )
+#        ax_true.set_ylabel("coverage")
+#    fig.legend(handles=[dupe_rate_lines, coverage_lines], loc="upper right")
+#    fig.suptitle(f"{gene_id} - {gene_name}")
+#    fig.savefig(outdir / f"{gene_id}.{gene_name}.png", dpi=400)
+#    pyplot.close()
 
-    # Make the plot
-    fig, axes = pyplot.subplots(figsize=(5,1.5*len(sample_ids)), nrows=len(sample_ids), layout="constrained", sharex=True, sharey=False)
 
-    for (ax, sample_id) in zip(axes, sample_ids):
-        both = get_cov_and_dupe_rate(gene_id, sample_id)
-        mask = np.array(both['mask']).astype(bool)
-        masked_dupe_rate = np.array(both['dupe_rate']).astype(float)
-        masked_dupe_rate[~mask] = float("nan")
-        dupe_rate_lines, = ax.plot(both['loc'], masked_dupe_rate, color="b", label="dupe rate")
-        ax.set_ylabel(f"{sample_id}\nPCR dupe rate")
-        ax.set_xlabel("loc")
-        ax_true = ax.twinx()
-        coverage_lines, = ax_true.plot(both['loc'], both['cov'], color="k", label="coverage")
-        ax_true.set_ylabel("coverage")
-    fig.legend(handles=[dupe_rate_lines, coverage_lines], loc = "upper right")
-    fig.suptitle(f"{gene_id} - {gene_name}")
-    fig.savefig(outdir / f"{gene_id}.{gene_name}.png", dpi=400)
-    pyplot.close()
+### COMPUTE CORRELATION STATS
+def weighted_gaussian_filter(x, weights, sigma, mode):
+    # also pulls towards the mean very slightly
+    # we only use the values where cov > 100 so this makes little difference
+    pseudo_count = 0.1
+    prior = np.sum(x * weights) / np.sum(weights)
+    weighted_smoothed = scipy.ndimage.gaussian_filter1d(
+        x * weights, sigma=sigma, mode=mode
+    )
+    weights = scipy.ndimage.gaussian_filter1d(weights, sigma=sigma, mode=mode)
+    return (weighted_smoothed + prior * pseudo_count) / (weights + pseudo_count)
+
+
+temp = []
+for (sample_id, transcript_id), transcript_data in data.group_by(
+    ["sample_id", "transcript_id"]
+):
+    cov = transcript_data["cov"].to_numpy().astype(float)
+    dupe_rate = transcript_data["dupe_rate"].to_numpy().astype(float)
+    mask = np.array(transcript_data["mask"]).astype(bool)
+
+    either_mask = mask[:-1] | mask[1:]
+
+    corr = np.corrcoef(cov[mask], dupe_rate[mask])[0, 1]
+    diff_corr = np.corrcoef(
+        np.diff(cov)[either_mask],
+        np.diff(dupe_rate)[either_mask],
+    )[0, 1]
+
+    smoothed_cov = scipy.ndimage.gaussian_filter1d(cov, sigma=30, mode="constant")
+    dupe_rate2 = dupe_rate.copy()
+    dupe_rate2[np.isnan(dupe_rate2)] = 0
+    smoothed_dupe_rate = weighted_gaussian_filter(
+        dupe_rate2,
+        weights=cov,
+        sigma=30,
+        mode="constant",
+    )
+    smoothed_corr = np.corrcoef(smoothed_cov[mask], smoothed_dupe_rate[mask])[0, 1]
+    smoothed_diff_corr = np.corrcoef(
+        np.diff(smoothed_cov)[either_mask],
+        np.diff(smoothed_dupe_rate)[either_mask],
+    )[0, 1]
+    temp.append(
+        {
+            "sample_id": sample_id,
+            "cycle_count": cycle_counts_by_id[sample_id],
+            "transcript_id": transcript_id,
+            "corr": corr,
+            "diff_corr": diff_corr,
+            "smoothed_corr": smoothed_corr,
+            "smoothed_diff_corr": smoothed_diff_corr,
+        }
+    )
+
+results = pl.DataFrame(temp).melt(
+    id_vars=["sample_id", "cycle_count", "transcript_id"],
+    variable_name="corr type",
+    value_name="corr",
+)
+
+
+# def wilcox(data):
+#    return scipy.stats.wilcoxon(data).pvalue
+#
+#
+# print(
+#    results.group_by(["cycle_count", "corr type"]).agg(
+#        pl.col("corr").median().alias("median corr"),
+#        pl.col("corr").min().alias("min corr"),
+#        pl.col("corr").max().alias("max corr"),
+#        pl.col("corr").map_batches(wilcox).alias("wilcox p"),
+#    )
+# )
+
+### PLOT CORRELATION STATS
+fig, ax = pyplot.subplots(figsize=(3, 2), sharey=True, constrained_layout=True)
+for i, sample_id in enumerate(sample_ids):
+    df = results.filter(
+        pl.col("corr type") == "smoothed_diff_corr",
+        sample_id=sample_id,
+    )
+    ax.scatter(i + np.random.random(size=len(df["corr"])) * 0.6 - 0.3, df["corr"])
+ax.set_xticks(range(len(sample_ids)))
+ax.set_xticklabels([cycle_counts_by_id[sample_id] for sample_id in sample_ids])
+ax.set_ylim(-1, 1)
+ax.set_ylabel("correlation")
+ax.set_xlabel("PCR cycle number")
+fig.savefig(outdir / "pcr_dupe_rate_correlation.png", dpi=300)
