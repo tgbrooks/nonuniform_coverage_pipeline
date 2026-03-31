37f3d3746fb2772ec98b318399999e1ddfcc3238
tgbrooks <tgbrooks@gmail.com>
2026-03-31 15:35:41 -0400

    Update 3prime correction scripts

--- src/scripts/correct_coverage_for_3prime.R
+++ src/scripts/correct_coverage_for_3prime.R
@@ -2,36 +2,31 @@ library(tidyverse)
 ## Correct for 3' bias by modelling coverage depth as a function
 ## of the distance from the 3' end, for each sample
 
-s <- list()
-for(sample_id in snakemake@params$sample_ids){
-  file <- paste0("results/alpine_fits/", snakemake@wildcards$tissue, "/", sample_id, ".coverage_table.txt")
-  temp <- read_tsv(file) |>
-    mutate(log_actual = log(actual+1), sample_id = sample_id) |>
-    group_by(gene) |>
-    mutate(dist = n() - pos + 1) |>
-    ungroup()
+file <- snakemake@input$cov
+sample_id = snakemake@wildcards$sample_id
 
-  train <- temp |>
-    group_by(gene) |>
-    # Trim 5' and 3' edges since coverage is often funky there
-    filter(pos >= min(pos) + min(100, n()/5) , pos <= max(pos) - min(100, n()/5)) |>
-    ungroup()
+results <- read_tsv(file) |>
+mutate(log_cov = log(cov+1), sample_id = sample_id) |>
+group_by(gene) |>
+mutate(dist = n() - pos + 1) |>
+ungroup()
 
-  # Model expression across all genes as a function of
-  # distance from the 3' end, on a log scale (so exponential fit)
-  model <- lm(data = train, log_actual ~ dist)
+train <- results |>
+group_by(gene) |>
+# Trim 5' and 3' edges since coverage is often funky there
+filter(pos >= min(pos) + min(100, n()/5) , pos <= max(pos) - min(100, n()/5)) |>
+ungroup()
 
-  temp$predicted <- predict(model, newdata = temp)
+# Model expression across all genes as a function of
+# distance from the 3' end, on a log scale (so exponential fit)
+model <- lm(data = train, log_cov ~ dist)
 
-  temp <- temp |>
-    mutate(predicted = exp(predicted)) |>
-    group_by(gene) |>
-    # Correct the data based off fit values
-    mutate(corrected = actual * mean(actual)/predicted)
+results$predicted <- predict(model, newdata = results)
 
-  s[[length(s) + 1]] <- temp
-}
-
-results <- bind_rows(s)
+results <- results |>
+mutate(predicted = exp(predicted)) |>
+group_by(gene) |>
+# Correct the data based off fit values
+mutate(corrected = cov * mean(cov)/predicted)
 
 results |> write_tsv(snakemake@output$corrected)
