1622ba1e5530dc114935c3feeffe6cea5fde639c
tgbrooks <tgbrooks@gmail.com>
2026-04-17 11:14:14 -0400

    Fix error in rnafold - was using test+train genes

--- src/scripts/run_alpine_rnafold.R
+++ src/scripts/run_alpine_rnafold.R
@@ -11,15 +11,27 @@ library(r2r)
 #ensembldb_sqlite <- "data/Mus_musculus.GRCm38.102.gtf.sqlite"
 #bam.files <- list("data/SRX16386863/bam/Aligned.sortedByCoord.out.bam")
 #transcript_file <- "data/liver/high_expressed_single_isoform_genes.txt"
+sample_id <- "SRX16386864"
+tissue <- "liver"
+bam.files <- list(paste0("data/", sample_id, "/bam/Aligned.sortedByCoord.out.bam"))
+bai <- paste0("data/", sample_id, "/bam/Aligned.sortedByCoord.out.bam.bai")
+ensembldb_sqlite <- "data/Mus_musculus.GRCm38.102.gtf.sqlite"
+transcript_file <- paste0("data/", tissue, "/training_set_transcripts.txt")
+fragtypes_file <- paste0("data/", tissue, "/alpine_rnafold.fragtypes.rda")
+BSgenome <- "BSgenome.Mmusculus.UCSC.mm10"
+readlength <- 151
+modelfile <- "temp.alpine_model.Rda"
 
 RNA_FOLD_RESOLUTION <- 25
 
 sample_id <- snakemake@wildcards$sample_id
 readlength <- snakemake@params$readlength 
-modelfile <- snakemake@output$modelfile
 ensembldb_sqlite <- snakemake@input$ensdb
 bam.files <- list(snakemake@input$bam)
 transcript_file <- snakemake@input$transcripts
+fragtypes_file <- snakemake@input$fragtypes
+BSgenome <- snakemake@params$BSgenome
+modelfile <- snakemake@output$modelfile
 
 names(bam.files) <- sample_id
 
@@ -44,8 +56,8 @@ gene.names <- names(ebt.fit)
 names(gene.names) <- gene.names
 
 # Load the BSgenome for our species
-library(snakemake@params$BSgenome, character.only=TRUE)
-if (snakemake@params$BSgenome == "BSgenome.Hsapiens.UCSC.hg38") {
+library(BSgenome, character.only=TRUE)
+if (BSgenome == "BSgenome.Hsapiens.UCSC.hg38") {
     my_genome <- BSgenome.Hsapiens.UCSC.hg38
 } else {
     my_genome <- BSgenome.Mmusculus.UCSC.mm10
@@ -53,12 +65,12 @@ if (snakemake@params$BSgenome == "BSgenome.Hsapiens.UCSC.hg38") {
 seqlevelsStyle(my_genome) <- "NCBI"
 
 
-fragtypes <- readRDS(snakemake@input$fragtypes)
+fragtypes <- readRDS(fragtypes_file)
 
 # Fix the readlength of the fragtypes
 # To avoid repeated work, we only compute fragtypes once and update its readlength to match the study
 fragtypes <- sapply(
-    names(fragtypes),
+    gene.names,
     function(gene.name) {
       ft <- fragtypes[[gene.name]]
       exons <- ebt.fit[[gene.name]]
